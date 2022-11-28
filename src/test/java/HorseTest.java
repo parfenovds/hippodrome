25717d4f981ce6636c84adad31a7687531ace2b7
Dmitrii Parfenov <parfenovds@protonmail.com>
2022-11-28 21:26:34 +0300

    add all horse tests

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,44 +1,66 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class HorseTest {
-    TestUtils testUtils = new TestUtils();
     private Horse threeParametersHorse;
     private Horse twoParametersHorse;
 
+
     @BeforeAll
-    void beforeAll() {
+    void createTwoHorsesForTests() {
         threeParametersHorse = new Horse(TestUtils.DEFAULT_NAME, TestUtils.DEFAULT_SPEED, TestUtils.DEFAULT_DISTANCE);
         twoParametersHorse = new Horse(TestUtils.DEFAULT_NAME, TestUtils.DEFAULT_SPEED);
     }
 
     @Test
+    @Order(1)
     void getName() {
         assertEquals(TestUtils.DEFAULT_NAME, threeParametersHorse.getName());
         assertEquals(TestUtils.DEFAULT_NAME, twoParametersHorse.getName());
     }
 
     @Test
+    @Order(2)
     void getSpeed() {
         assertEquals(TestUtils.DEFAULT_SPEED, threeParametersHorse.getSpeed());
         assertEquals(TestUtils.DEFAULT_SPEED, twoParametersHorse.getSpeed());
     }
 
     @Test
+    @Order(3)
     void getDistance() {
         assertEquals(TestUtils.DEFAULT_DISTANCE, threeParametersHorse.getDistance());
         assertEquals(0, twoParametersHorse.getDistance());
     }
 
     @Test
-    void move() {
-
+    @Order(4)
+    void parametersOfGetRandomDouble() {
+        try (MockedStatic<Horse> mockedStatic = Mockito.mockStatic(Horse.class)) {
+            threeParametersHorse.move();
+            mockedStatic.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+        }
     }
 
     @Test
-    void getRandomDouble() {
+    @Order(5)
+    void resultOfGetRandomDouble() {
+        try (MockedStatic<Horse> mockedStatic = Mockito.mockStatic(Horse.class)) {
+            mockedStatic.when(() -> Horse.getRandomDouble(Mockito.anyDouble(), Mockito.anyDouble())).thenReturn(1D);
+            twoParametersHorse.move();
+            assertEquals(twoParametersHorse.getSpeed(), twoParametersHorse.getDistance());
+            mockedStatic.when(() -> Horse.getRandomDouble(Mockito.anyDouble(), Mockito.anyDouble())).thenReturn(0D);
+            twoParametersHorse.move();
+            assertEquals(twoParametersHorse.getSpeed(), twoParametersHorse.getDistance());
+            mockedStatic.when(() -> Horse.getRandomDouble(Mockito.anyDouble(), Mockito.anyDouble())).thenReturn(1D);
+            twoParametersHorse.move();
+            assertEquals(twoParametersHorse.getSpeed() * 2, twoParametersHorse.getDistance());
+        }
     }
 }
